f147ff494c985bdf3e5fa3d82cf14c58b4cc22ad
redVi <liberty195@gmail.com>
2021-07-21 21:03:12 +0700

    pipeline-operator

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { lazy, Suspense } from 'react';
 import styles from './home.module.css';
 import car from 'assets/images/car.jpg';
+import { capitalize } from 'utils/capitalize';
 const Title = lazy(() => import(/* webpackChunkName: 'TitleComponent' */ 'components/Title'));
 
 export default function HomePage () {
@@ -8,6 +9,7 @@ export default function HomePage () {
     <Suspense fallback={<div>Loading...</div>}>
       <div className={styles.home}>
         <Title />
+        <h2>{'yello' |> capitalize}</h2>
         <img src={car} alt="Car" />
       </div>
     </Suspense>
